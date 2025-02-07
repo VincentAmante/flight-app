6aaf1eae5032c6e6b9b2e6fcebe8c580d1c8aa55
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-02-07 21:35:52 +0400

    Update AppHeader.tsx
    Part of earlier app rebrand from 'Spotter'

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -1,18 +1,34 @@
 'use client'
 
-import { AppBar, Typography, Container } from '@mui/material';
+import Image from 'next/image';
+import {
+    AppBar,
+    Typography,
+    Container, 
+    Stack,
+} from '@mui/material';
 
 function AppHeader() {
 
     return (
-        <AppBar elevation={1} position="static" color='transparent'>
-            <Container sx={{ py: 1 }}>
-                <Typography
-                    variant="h6"
-                    noWrap
-                >
-                    Spotter
-                </Typography>
+        <AppBar elevation={1} position="static" color='transparent' sx={{
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+            backdropFilter: 'blur(1px)',
+            borderBottom: '1px solid rgba(255, 255, 255, 0.4)',
+        }}>
+            <Container sx={{ py: 2 }}>
+                <Stack direction='row' spacing={1}>
+                    <Image src="/toppers.png" width={36} height={36} alt='Logo' className='select-none' />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        fontFamily='Montserrat'
+                        color='white' 
+                        className='select-none'
+                    >
+                        SkyTopper
+                    </Typography>
+                </Stack>
             </Container>
         </AppBar>
     );
