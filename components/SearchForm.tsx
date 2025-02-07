d1a43d1470d691d59819ba160b85d56e823132fc
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-02-07 17:47:40 +0400

    Made form look a little nicer

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -4,6 +4,9 @@ import {
     TextField,
     Button,
     Grid2,
+    Paper,
+    Container,
+    Box
 } from '@mui/material'
 
 import { Search as SearchIcon } from '@mui/icons-material'
@@ -17,7 +20,6 @@ import { zodResolver } from '@hookform/resolvers/zod'
 
 import { QuerySchema, QuerySchemaType } from '../utils/airscrapper-schemas'
 import { useState } from 'react'
-
 interface SearchFormProps {
     onSubmit?: (data: QuerySchemaType) => void
 }
@@ -37,87 +39,87 @@ export default function SearchForm(props: SearchFormProps) {
     const [fromDate, setFromDate] = useState(new Date().toISOString().split('T')[0])
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <Grid2 container rowSpacing={1} columnSpacing={2}
-                columns={{
-                    xs: 1,
-                    sm: 2,
-                    md: 4
-                }}
-            >
-                <Grid2 size={1}>
-                    <TextField
-                        {...register('origin')}
-                        label="Origin" variant="outlined"
-                        error={!!errors.origin}
-                        helperText={errors.origin?.message || ''}
-                        fullWidth
-                    />
-                </Grid2>
-                <Grid2 size={1}>
-                    <TextField
-                        {...register('destination')}
-                        label="Destination" variant="outlined"
-                        error={!!errors.destination}
-                        helperText={errors.destination?.message || ''}
-                        fullWidth
-                    />
-                </Grid2>
-                <Grid2 size={1}>
-                    <TextField
-                        {...register('dateRange.from')}
-                        type="date"
-                        label="From"
-                        slotProps={{
-                            inputLabel: {
-                                shrink: true,
-                            },
-                            htmlInput: {
-                                // Format the date to match the ISO format
-                                min: new Date().toISOString().split('T')[0]
-                            }
+        <Paper elevation={2}>
+            <Container sx={{ p: 1 }}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Grid2 container rowSpacing={2} columnSpacing={1}
+                        columns={{
+                            xs: 2,
+                            md: 4
                         }}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                            setFromDate(e.target.value);
-                        }}
-                        fullWidth
-                        error={!!errors.dateRange?.from}
-                        helperText={(errors.dateRange?.from) ? errors.dateRange.from.message : ''}
                     >
-                    </TextField>
-                </Grid2>
-                <Grid2 size={1}
-                    direction='column'
-                >
-                    <TextField
-                        {...register('dateRange.to')}
-                        type="date"
-                        label="To"
-                        slotProps={{
-                            inputLabel: {
-                                shrink: true,
-                            },
-                            htmlInput: {
-                                // Musn't be before the from date
-                                min: fromDate
-                            }
-                        }}
-                        fullWidth
-                        error={!!errors.dateRange?.to}
-                        helperText={(errors.dateRange?.to) ? errors.dateRange.to.message : ''}
-                    ></TextField>
-                </Grid2>
-                <Grid2 size={2} sx={{
-                    py: 1
-                }}
-                    display='flex'
-                    justifyContent='center'
-                >
-                    <Button
-                        onClick={handleSubmit(onSubmit)}
-                        startIcon={<SearchIcon />} size='large' variant='contained' color='primary'>Search</Button>
-                </Grid2>
-            </Grid2>
-        </form>
+                        <Grid2 size={1}>
+                            <TextField
+                                {...register('origin')}
+                                label="Origin" variant="outlined"
+                                error={!!errors.origin}
+                                helperText={errors.origin?.message || ''}
+                                fullWidth
+                            />
+                        </Grid2>
+                        <Grid2 size={1}>
+                            <TextField
+                                {...register('destination')}
+                                label="Destination" variant="outlined"
+                                error={!!errors.destination}
+                                helperText={errors.destination?.message || ''}
+                                fullWidth
+                            />
+                        </Grid2>
+                        <Grid2 size={1}>
+                            <TextField
+                                {...register('dateRange.from')}
+                                type="date"
+                                label="Departure"
+                                slotProps={{
+                                    inputLabel: {
+                                        shrink: true,
+                                    },
+                                    htmlInput: {
+                                        // Format the date to match the ISO format
+                                        min: new Date().toISOString().split('T')[0]
+                                    }
+                                }}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                    setFromDate(e.target.value);
+                                }}
+                                fullWidth
+                                error={!!errors.dateRange?.from}
+                                helperText={(errors.dateRange?.from) ? errors.dateRange.from.message : ''}
+                            >
+                            </TextField>
+                        </Grid2>
+                        <Grid2 size={1}
+                            direction='column'
+                        >
+                            <TextField
+                                {...register('dateRange.to')}
+                                type="date"
+                                label="Return"
+                                slotProps={{
+                                    inputLabel: {
+                                        shrink: true,
+                                    },
+                                    htmlInput: {
+                                        // Musn't be before the from date
+                                        min: fromDate
+                                    }
+                                }}
+                                fullWidth
+                                error={!!errors.dateRange?.to}
+                                helperText={(errors.dateRange?.to) ? errors.dateRange.to.message : ''}
+                            ></TextField>
+                        </Grid2>
+                    </Grid2>
+                    <Grid2 size={4} sx={{ pt: 1 }} display='flex' justifyContent='center'>
+                        <Button
+                            onClick={handleSubmit(onSubmit)}
+                            startIcon={<SearchIcon />} size='large' variant='contained' color='primary'>
+                            Search
+                        </Button>
+                    </Grid2>
+                </form>
+            </Container>
+        </Paper>
     )
 }
