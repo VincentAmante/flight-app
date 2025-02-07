43950cfe54bbe2d82964a360f4027ccd76c2cfd7
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-02-07 17:47:44 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
 'use client'
 
-import { Container, } from '@mui/material'
+import { Container, Stack, } from '@mui/material'
 import SearchForm from '../../components/SearchForm';
 import { useEffect, useState } from 'react';
-import { getFlights } from './api/actions';
+import { getFlights, getFlightsMock } from './api/actions';
 import { QuerySchemaType } from '../../utils/airscrapper-schemas';
+import FlightsDisplay from '../../components/FlightsDisplay';
+import { ItinerariesEntity } from '../../utils/airscrapper-types';
 
 export default function Home() {
   const [search, setSearch] = useState<QuerySchemaType | null>(null);
+  const [itineraries, setItineraries] = useState<ItinerariesEntity[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchFlights() {
-      if (search)
-        await getFlights(search).then((res) => {
+      if (search){
+        setIsLoading(true);
+        await getFlightsMock(search).then((res) => {
           console.log(res);
-        })
+
+          if (res.flights && res.flights.itineraries)
+            setItineraries(res.flights.itineraries)
+        }).then(() => setIsLoading(false));
+      }
     }
 
     fetchFlights();
   }, [search])
 
   return (
-    <Container>
-      <SearchForm onSubmit={(data) => setSearch(data)} />
+    <Container sx={{ py: 2 }}>
+      <Stack spacing={2}>
+        <SearchForm onSubmit={(data) => setSearch(data)} />
+        <FlightsDisplay itineraries={itineraries} isLoading={isLoading} />
+      </Stack>
     </Container>
   );
 }
