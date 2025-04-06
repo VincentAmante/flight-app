2facfb573738f7ceed2ac1bb2a9f052fc51da00e
Vincent Amante <71628473+VincentAmante@users.noreply.github.com>
2025-04-06 21:19:30 +0400

    Update page.tsx
    + Added error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,35 +11,46 @@ import Image from 'next/image';
 
 export default function Home() {
   const [search, setSearch] = useState<QuerySchemaType | null>(null);
+  const [error, setError] = useState('')
   const [itineraries, setItineraries] = useState<ItinerariesEntity[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [hasNoFlightsFound, setNoFlightsFound] = useState(false);
 
+
   useEffect(() => {
+    // src/app/page.tsx
     async function fetchFlights() {
-      if (search){
+      if (search) {
         setIsLoading(true);
-        await getFlights(search).then((res) => {
-          if (res.flights && res.flights.itineraries){
-            setItineraries(res.flights.itineraries)
-            if (res.flights.itineraries.length === 0){
+        try {
+          const res = await getFlights(search);
+          if (res.flights && res.flights.itineraries) {
+            setItineraries(res.flights.itineraries);
+            if (res.flights.itineraries.length === 0) {
               setNoFlightsFound(true);
             } else {
               setNoFlightsFound(false);
             }
           }
-        }).finally(() => setIsLoading(false));
+        } catch (error) {
+          console.error('Error fetching flights:', error);
+          setError('Failed to fetch flights. Please try again later.');
+        } finally {
+          setIsLoading(false);
+        }
       }
     }
 
-    fetchFlights();
+    fetchFlights()
   }, [search])
 
   return (
-    <Container sx={{ py: 2, px: {
-      xs: 1,
-      md: 4,
-    } }}>
+    <Container sx={{
+      py: 2, px: {
+        xs: 1,
+        md: 4,
+      }
+    }}>
       <Image src='/splash.png' width={1720} height={800} alt="AirScrapper" className='hidden lg:block absolute top-0 left-0 w-full -z-10 select-none' />
       <Image src='/splash-tablet.png' width={750} height={1334} alt="AirScrapper" className='hidden sm:block lg:hidden absolute top-0 left-0 w-full -z-10 select-none' />
       <Image src='/splash-mobile.png' width={750} height={1334} alt="AirScrapper" className='block sm:hidden absolute top-0 left-0 w-full -z-10 select-none' />
